eced3f51687d023d8b8a0f03f994b48fc69aaf77
Zach Rodis <zach@Zachs-MacBook-Air.local>
2020-03-22 16:09:57 -0400

    started mobile nav

---
+++ pages/donate.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
+export default function Donate() {
+    return (
+        <Container maxWidth='sm'>
+            <Box my={4}>
+                <Typography variant='h1' component='h1' gutterBottom>
+                    donate
+                </Typography>
+                <Typography variant='h2' component='h1' gutterBottom>
+                    donate
+                </Typography>
+                <Typography variant='h3' component='h1' gutterBottom>
+                    donate
+                </Typography>
+                <Typography variant='h4' component='h1' gutterBottom>
+                    donate
+                </Typography>
+            </Box>
+        </Container>
+    )
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import Link from 'next/link'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import { List, ListItem, ListItemText, Divider } from '@material-ui/core'
 
 const useStyles = makeStyles({
     root: {
@@ -19,8 +21,47 @@ const useStyles = makeStyles({
     },
 })
 
+const MobileNavDrawer = ({ isOpen, handleToggle }: { isOpen: boolean; handleToggle(): void }) => {
+    const classes = useStyles()
+
+    return (
+        <SwipeableDrawer anchor={'left'} open={isOpen} onOpen={handleToggle} onClose={handleToggle}>
+            <List>
+                <ListItem button key={'Home'}>
+                    <Link href='/'>
+                        <Typography variant='h6' className={classes.title}>
+                            Home
+                        </Typography>
+                    </Link>
+                </ListItem>
+                <Divider />
+                <ListItem button key={'Health & Educaiton'}>
+                    <Link href='/health-education'>
+                        <Typography variant='h6' className={classes.title}>
+                            Health & Educaiton
+                        </Typography>
+                    </Link>
+                </ListItem>
+                <Divider />
+                <ListItem button key={'Donate'}>
+                    <Link href='/donate'>
+                        <Typography variant='h6' className={classes.title}>
+                            Donate
+                        </Typography>
+                    </Link>
+                </ListItem>
+            </List>
+        </SwipeableDrawer>
+    )
+}
+
 export const NavBar = () => {
     const classes = useStyles()
+    const [isDrawerOpen, toggleDrawer] = useState(false)
+
+    const handleMobileDrawer = () => {
+        toggleDrawer(!isDrawerOpen)
+    }
 
     return (
         <div className={classes.root}>
@@ -31,22 +72,45 @@ export const NavBar = () => {
                         className={classes.menuButton}
                         color='inherit'
                         aria-label='menu'
+                        onClick={handleMobileDrawer}
                     >
                         <MenuIcon />
+                        <MobileNavDrawer isOpen={isDrawerOpen} handleToggle={handleMobileDrawer} />
                     </IconButton>
-                    <Link href='/'>
-                        <Typography variant='h6' className={classes.title}>
-                            Home
-                        </Typography>
-                    </Link>
-                    <Link href='/donations'>
-                        <Typography variant='h6' className={classes.title}>
-                            Donate
-                        </Typography>
-                    </Link>
+
                     <Button color='inherit'>Login</Button>
                 </Toolbar>
             </AppBar>
         </div>
+    )
+}
+
+interface NavLink {
+    path: string
+    title: string
+}
+const generateNavLinks = (props: { links: NavLink[] }) => {
+    const classes = useStyles()
+
+    return props.links.map(({ path, title }) => (
+        <Link href={path}>
+            <Typography variant='h6' className={classes.title}>
+                {title}
+            </Typography>
+        </Link>
+    ))
+}
+
+const DesktopNav = () => {
+    const classes = useStyles()
+
+    return (
+        <>
+            <Link href='/donate'>
+                <Typography variant='h6' className={classes.title}>
+                    Donate
+                </Typography>
+            </Link>
+        </>
     )
 }
